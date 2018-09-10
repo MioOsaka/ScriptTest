4063b674d736bcc8b92dc29ab4722e41efa3f237
MioOsaka <mosaka@ths.co.jp>
2018-09-10 15:57:07 +0900

    Lesson4の課題提出です チェック宜しくお願いします。

---
+++ Assets/Test.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// 配列を初期化する
+		int[] points = {1, 2, 3, 4, 5};
+
+		// 配列の要素数のぶんだけ処理を繰り返す
+		for (int i = 0; i < points.Length; i++) {
+			// 配列の要素を順番に表示する
+			Debug.Log (points [i]);
+		}
+			// 配列の要素数のぶんだけ処理を繰り返す
+		for (int i = points.Length - 1; 0 <= i ; i--){
+			// 配列の要素を逆順番に表示する
+			Debug.Log (points [i]);	
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Test2.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss {        
+
+	private int mp = 53; //マジックポイント
+
+	//Magic関数
+	public void Magic(){
+		int decmp = this.mp - 5;
+
+		if (decmp < 0) {
+			//mpが0未満の場合
+			Debug.Log ("MPが足りないため魔法が使えない");
+		} else {
+			//mpが0以上の場合
+			Debug.Log ("魔法攻撃をした 残りMPは" + decmp);
+		}
+		//残りmpを減らす
+		this.mp = decmp;
+		}
+}
+
+
+public class Test2 : MonoBehaviour {
+
+	void Start () {
+		// Bossクラスの変数を宣言してインスタンスを代入
+		Boss lastboss = new Boss ();
+
+		// 11回まで処理を繰り返す
+		for (int i = 0; i < 11; i++) {
+			// Magic関数を呼び出す
+			lastboss.Magic();
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
